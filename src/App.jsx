5246cbe4c594f21aad3c2bb9e3681037a785643d
PhyoWunnaLin <phyowunnalin1214@gmail.com>
2023-07-14 18:25:59 +0630

    error

--- src/App.jsx
+++ src/App.jsx
@@ -50,20 +50,20 @@ const App = () => {
     // bg-[rgb(206,205,194)]  bg-[rgb(24,24,24)]
 
     <div className=" relative bg-[rgb(24,24,24)] ">
-      {/* {<Navbar />} */}
+      {<Navbar />}
 
-      {/* <Menu /> */}
-      {/* <section id="0" className=" relative">
+      <Menu />
+      <section id="0" className=" relative">
         <Landing />
-      </section> */}
+      </section>
 
       <section id="0" className=" relative">
         <About />
       </section>
 
-      {/* <section className=" relative">
+      <section className=" relative">
         <Education />
-      </section> */}
+      </section>
 
       <section id="1" className=" relative">
         <Skill />
