89241b94058dcc9e9d86c31c34fcf5199a01069c
PhyoWunnaLin <phyowunnalin1214@gmail.com>
2023-07-14 15:58:50 +0630

    contact responsive

--- src/components/sections/contact/Contact.jsx
+++ src/components/sections/contact/Contact.jsx
@@ -93,10 +93,10 @@ const Contact = () => {
                     Address :
                   </h1>
                   <div className="flex flex-col gap-1">
-                    <p className=" text-pink-400 font-sans tracking-wider text-sm">
+                    <p className=" text-pink-500 font-sans tracking-wider text-sm">
                       Myanmar , Yangon
                     </p>
-                    <p className=" text-pink-400 font-sans tracking-wider text-sm">
+                    <p className=" text-pink-500 font-sans tracking-wider text-sm">
                       North Dagon
                     </p>
                   </div>
@@ -106,11 +106,11 @@ const Contact = () => {
                     Contact me directly :
                   </h1>
                   <div className="flex flex-col gap-1">
-                    <p className=" text-pink-400 font-sans tracking-wider text-sm flex gap-2 items-center justify-start">
+                    <p className=" text-pink-500 font-sans tracking-wider text-sm flex gap-2 items-center justify-start">
                       <BsFillPhoneVibrateFill className=" text-lg text-pink-500" />{" "}
                       +47 333 78 901
                     </p>
-                    <p className=" text-pink-400 font-sans tracking-wider text-sm flex gap-2 justify-start items-center">
+                    <p className=" text-pink-500 font-sans tracking-wider text-sm flex gap-2 justify-start items-center">
                       <MdMarkEmailRead className=" text-lg text-pink-500" />{" "}
                       american@gmail.com
                     </p>
@@ -166,61 +166,61 @@ const Contact = () => {
 
       {/* ipad view  */}
       <div
-        className={`flex items-center justify-around bg-[#292923] h-[500px] max-[599px]:hidden min-[900px]:hidden`}>
+        className={`flex items-center justify-around h-[500px] max-[599px]:hidden min-[900px]:hidden`}>
         {/* information  */}
         <div className="flex flex-col mx-4 gap-4">
-          <h1 className="font-sans text-3xl tracking-wider text-gray-50 font-medium">
+          <h1 className="font-sans text-3xl tracking-wider text-yellow-500 font-medium">
             Portfolio
           </h1>
           <div className="flex flex-col gap-4">
             <div className="flex flex-col justify-start">
-              <h1 className=" font-sans text-xl mb-1 text-gray-50 font-medium">
+              <h1 className=" font-sans text-xl mb-1 text-cyan-500 font-medium">
                 Address :
               </h1>
-              <p className=" text-gray-200 font-sans tracking-wider text-sm">
+              <p className=" text-pink-500 font-sans tracking-wider text-sm">
                 Myanmar , Yangon
               </p>
-              <p className=" text-gray-200 font-sans tracking-wider text-sm">
+              <p className=" text-pink-500 font-sans tracking-wider text-sm">
                 North Dagon
               </p>
             </div>
             <div className="flex flex-col justify-start">
-              <h1 className=" font-sans text-xl mb-2 text-gray-50 font-medium">
+              <h1 className=" font-sans text-xl mb-2 text-cyan-500 font-medium">
                 Contact me directly :
               </h1>
-              <p className=" text-gray-200 font-sans tracking-wider text-sm flex gap-2 items-center justify-start">
-                <BsFillPhoneVibrateFill className=" text-lg text-[#6a9c78]" />{" "}
+              <p className=" text-pink-500 font-sans tracking-wider text-sm flex gap-2 items-center justify-start">
+                <BsFillPhoneVibrateFill className=" text-lg text-pink-500" />{" "}
                 +47 333 78 901
               </p>
-              <p className=" text-gray-200 font-sans tracking-wider text-sm flex gap-2 justify-start items-center">
-                <MdMarkEmailRead className=" text-lg text-[#6a9c78]" />{" "}
+              <p className=" text-pink-500 font-sans tracking-wider text-sm flex gap-2 justify-start items-center">
+                <MdMarkEmailRead className=" text-lg text-pink-500" />{" "}
                 american@gmail.com
               </p>
             </div>
           </div>
           <div className="flex flex-col gap-4 justify-start">
-            <p className=" text-xl font-sans font-medium text-gray-50">
+            <p className=" text-xl font-sans font-medium text-cyan-500">
               Check my profiles
             </p>
             <div className="flex gap-5 justify-start">
               <a
                 href="#"
-                className=" w-8 h-8 rounded-full bg-[#5c7658] hover:bg-[#6a9c78] duration-500 text-gray-50 flex justify-center items-center">
+                className=" w-8 h-8 rounded-full form-btn duration-500 text-gray-50 flex justify-center items-center">
                 <AiFillGithub className=" text-lg" />
               </a>
               <a
                 href="#"
-                className=" w-8 h-8 rounded-full bg-[#5c7658] hover:bg-[#6a9c78] duration-500 text-gray-50 flex justify-center items-center">
+                className=" w-8 h-8 rounded-full form-btn duration-500 text-gray-50 flex justify-center items-center">
                 <BsFacebook className=" text-lg" />
               </a>
               <a
                 href="#"
-                className=" w-8 h-8 rounded-full bg-[#5c7658] hover:bg-[#6a9c78] duration-500 text-gray-50 flex justify-center items-center">
+                className=" w-8 h-8 rounded-full form-btn duration-500 text-gray-50 flex justify-center items-center">
                 <AiFillInstagram className=" text-lg" />
               </a>
               <a
                 href="#"
-                className=" w-8 h-8 rounded-full bg-[#5c7658] hover:bg-[#6a9c78] duration-500 text-gray-50 flex justify-center items-center">
+                className=" w-8 h-8 rounded-full form-btn duration-500 text-gray-50 flex justify-center items-center">
                 <SiNetlify className=" text-lg" />
               </a>
             </div>
@@ -259,7 +259,7 @@ const Contact = () => {
             </div>
             <button
               onClick={() => setShow(!show)}
-              className=" bg-[#5c7658] hover:bg-[#6a9c78] duration-500 text-gray-50 rounded-md font-bold font-sans py-3">
+              className=" form-btn duration-500 text-gray-50 rounded-md font-bold font-sans py-3">
               Submit
             </button>
           </form>
@@ -267,22 +267,8 @@ const Contact = () => {
       </div>
 
       {/* phone view */}
-      <div className=" w-full min-[600px]:hidden">
-        <div className=" w-full h-[600px] relative">
-          <img
-            src={laptop}
-            className="w-full h-[600px] object-cover absolute top-0 z-[-50]"
-          />
-          <p className=" text-5xl pt-[145px] font-sans font-bold text-white text-center tracking-wide">
-            Let's work together
-          </p>
-          <div className="box">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-        <form className="w-full py-7 flex items-center justify-center bg-[#292923]">
+      <div className=" w-full mb-10 min-[600px]:hidden">
+        <form className="w-full py-7 flex items-center justify-center">
           <div className={`flex flex-col mx-4 gap-7`}>
             <div className="form1">
               <input
@@ -313,65 +299,65 @@ const Contact = () => {
             </div>
             <button
               onClick={() => setShow(!show)}
-              className=" bg-[#5c7658] hover:bg-[#6a9c78] duration-500 text-gray-50 rounded-md font-bold font-sans px-8 py-3">
+              className=" form-btn duration-500 text-gray-50 rounded-md font-bold font-sans px-8 py-3">
               Submit
             </button>
             <div className="flex flex-col gap-4">
-              <h1 className="font-sans text-3xl tracking-wider text-gray-50 font-medium text-center">
+              <h1 className="font-sans text-3xl tracking-wider text-yellow-500 font-medium text-center">
                 Portfolio
               </h1>
               <div className="flex flex-col gap-2">
-                <h1 className=" font-sans text-xl text-gray-50 font-medium text-center">
+                <h1 className=" font-sans text-xl text-cyan-500 font-medium text-center">
                   Address :
                 </h1>
                 <div className="flex flex-col gap-1">
-                  <p className=" text-gray-200 text-center font-sans tracking-wider text-sm">
+                  <p className=" text-pink-500 text-center font-sans tracking-wider text-sm">
                     Myanmar , Yangon
                   </p>
-                  <p className=" text-gray-200 text-center font-sans tracking-wider text-sm">
+                  <p className=" text-pink-500 text-center font-sans tracking-wider text-sm">
                     North Dagon
                   </p>
                 </div>
               </div>
               <div className="flex flex-col gap-2">
-                <h1 className=" font-sans text-xl text-gray-50 font-medium text-center">
+                <h1 className=" font-sans text-xl text-cyan-500 font-medium text-center">
                   Contact me directly :
                 </h1>
                 <div className="flex flex-col gap-1">
-                  <p className=" text-gray-200 font-sans tracking-wider text-sm flex gap-2 items-center justify-center">
-                    <BsFillPhoneVibrateFill className=" text-lg text-[#6a9c78]" />{" "}
+                  <p className=" text-pink-500 font-sans tracking-wider text-sm flex gap-2 items-center justify-center">
+                    <BsFillPhoneVibrateFill className=" text-lg text-pink-500" />{" "}
                     +47 333 78 901
                   </p>
-                  <p className=" text-gray-200 font-sans tracking-wider text-sm flex gap-2 justify-center items-center">
-                    <MdMarkEmailRead className=" text-lg text-[#6a9c78]" />{" "}
+                  <p className=" text-pink-500 font-sans tracking-wider text-sm flex gap-2 justify-center items-center">
+                    <MdMarkEmailRead className=" text-lg text-pink-500" />{" "}
                     american@gmail.com
                   </p>
                 </div>
               </div>
             </div>
             <div className="flex flex-col gap-4">
-              <p className=" text-xl font-sans font-medium text-center text-gray-50">
+              <p className=" text-xl font-sans font-medium text-center text-cyan-500">
                 Check my profiles
               </p>
               <div className="flex gap-6 justify-center">
                 <a
                   href="#"
-                  className=" w-8 h-8 rounded-full bg-[#5c7658] hover:bg-[#6a9c78] duration-500 text-gray-50 flex justify-center items-center">
+                  className=" w-8 h-8 rounded-full form-btn duration-500 text-gray-50 flex justify-center items-center">
                   <AiFillGithub className=" text-lg" />
                 </a>
                 <a
                   href="#"
-                  className=" w-8 h-8 rounded-full bg-[#5c7658] hover:bg-[#6a9c78] duration-500 text-gray-50 flex justify-center items-center">
+                  className=" w-8 h-8 rounded-full form-btn duration-500 text-gray-50 flex justify-center items-center">
                   <BsFacebook className=" text-lg" />
                 </a>
                 <a
                   href="#"
-                  className=" w-8 h-8 rounded-full bg-[#5c7658] hover:bg-[#6a9c78] duration-500 text-gray-50 flex justify-center items-center">
+                  className=" w-8 h-8 rounded-full form-btn duration-500 text-gray-50 flex justify-center items-center">
                   <AiFillInstagram className=" text-lg" />
                 </a>
                 <a
                   href="#"
-                  className=" w-8 h-8 rounded-full bg-[#5c7658] hover:bg-[#6a9c78] duration-500 text-gray-50 flex justify-center items-center">
+                  className=" w-8 h-8 rounded-full form-btn duration-500 text-gray-50 flex justify-center items-center">
                   <SiNetlify className=" text-lg" />
                 </a>
               </div>
